4dab21dc4138587b55a777ae34d686493eac2020
Gautham Vanjre <vanjregautham@gmail.com>
2022-07-12 20:05:23 +0530

    design of Home Fragment

--- app/src/main/java/com/example/notetakingapp/ui/Fragments/AddNotesFragment.kt
+++ app/src/main/java/com/example/notetakingapp/ui/Fragments/AddNotesFragment.kt
@@ -6,17 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.notetakingapp.R
+import com.example.notetakingapp.databinding.FragmentAddNotesBinding
 
 
 class AddNotesFragment : Fragment() {
 
 
+    lateinit var binding: FragmentAddNotesBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_notes, container, false)
+
+        binding = FragmentAddNotesBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
 
