7ac4f22ef262905b9971e0859f1c2af0f780245e
Gautham Vanjre <vanjregautham@gmail.com>
2022-07-14 12:25:29 +0530

    setup adapter to display notes in home fragment

--- app/src/main/java/com/example/notetakingapp/ui/Adapter/NotesAdapter.kt
+++ app/src/main/java/com/example/notetakingapp/ui/Adapter/NotesAdapter.kt
@@ -5,19 +5,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.notetakingapp.model.Notes
+import com.example.notetakingapp.R
 import com.example.notetakingapp.databinding.ListNotesBinding
+import com.example.notetakingapp.model.Notes
 
 class NotesAdapter( val requireContext: Context, val notesList: List<Notes>) : RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
 
-    class NotesViewHolder(val binding: ListNotesBinding) : RecyclerView.ViewHolder(binding.root)
+    class NotesViewHolder(val binding: ListNotesBinding):RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
-        return NotesViewHolder(ListNotesBinding.bind())
+        return NotesViewHolder(
+            ListNotesBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentNotesData = notesList[position]
+        holder.binding.notesTitle.text = currentNotesData.title
+        holder.binding.notesSubtitle.text = currentNotesData.subTitle
+        holder.binding.notesDate.text = currentNotesData.date
+        when(currentNotesData.priority){
+            "1" -> {
+                holder.binding.viewPriority.setBackgroundResource(R.drawable.red_dot)
+            }
+            "2" -> {
+                holder.binding.viewPriority.setBackgroundResource(R.drawable.yellow_dot)
+            }
+            "3" -> {
+                holder.binding.viewPriority.setBackgroundResource(R.drawable.green_dot)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
