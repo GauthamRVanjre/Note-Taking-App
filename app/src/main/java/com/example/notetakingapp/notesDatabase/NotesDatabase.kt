1ab2033d1ed269a95cd3d5ea130daa6df549a002
Gautham Vanjre <vanjregautham@gmail.com>
2022-07-14 11:37:16 +0530

    adding notes to database

--- app/src/main/java/com/example/notetakingapp/notesDatabase/NotesDatabase.kt
+++ app/src/main/java/com/example/notetakingapp/notesDatabase/NotesDatabase.kt
@@ -1,10 +1,13 @@
 package com.example.notetakingapp.notesDatabase
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.notetakingapp.dao.NotesDao
+import com.example.notetakingapp.model.Notes
 
+@Database(entities = [Notes::class], version = 1, exportSchema = false)
 abstract class NotesDatabase :RoomDatabase(){
 
     abstract fun notesDao(): NotesDao
--- app/src/main/java/com/example/notetakingapp/ui/Fragments/AddNotesFragment.kt
+++ app/src/main/java/com/example/notetakingapp/ui/Fragments/AddNotesFragment.kt
@@ -1,18 +1,27 @@
 package com.example.notetakingapp.ui.Fragments
 
 import android.os.Bundle
+import android.text.format.DateFormat
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import com.example.notetakingapp.R
 import com.example.notetakingapp.databinding.FragmentAddNotesBinding
+import com.example.notetakingapp.model.Notes
+import com.example.notetakingapp.viewmodel.NotesViewModel
+import java.util.*
 
 
 class AddNotesFragment : Fragment() {
 
 
     lateinit var binding: FragmentAddNotesBinding
+    var priority : String = "1"
+    val viewModel : NotesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +29,52 @@ class AddNotesFragment : Fragment() {
     ): View? {
 
         binding = FragmentAddNotesBinding.inflate(layoutInflater,container,false)
+        binding.pred.setImageResource(R.drawable.ic_done_btn)
+        binding.pred.setOnClickListener {
+            priority = "1"
+            binding.pred.setImageResource(R.drawable.ic_done_btn)
+            binding.pgreen.setImageResource(0)
+            binding.pyellow.setImageResource(0)
+        }
+        binding.pgreen.setOnClickListener {
+            priority = "3"
+            binding.pgreen.setImageResource(R.drawable.ic_done_btn)
+            binding.pred.setImageResource(0)
+            binding.pyellow.setImageResource(0)
+        }
+        binding.pyellow.setOnClickListener {
+            priority = "2"
+            binding.pyellow.setImageResource(R.drawable.ic_done_btn)
+            binding.pgreen.setImageResource(0)
+            binding.pred.setImageResource(0)
+        }
+        binding.btnSaveNotes.setOnClickListener { 
+            createNotes(it)
+
+        }
         return binding.root
     }
+
+    private fun createNotes(it: View?) {
+        val title = binding.editTitle.text.toString()
+        val subtitle = binding.editSubTitle.text.toString()
+        val notes = binding.editNotes.text.toString()
+
+        //getting current date
+        val d = Date()
+        val notesDate : CharSequence = DateFormat.format("MMMM d, yyyy ",d.time)
+        val notesData = Notes(
+             null,
+            title = title,
+            subTitle = subtitle,
+            notes = notes,
+            date = notesDate.toString(),
+            priority = priority
+        )
+        viewModel.addNotes(notesData)
+        Toast.makeText(this.context,"Note created successfully ",Toast.LENGTH_SHORT).show()
+    }
+
 
 
 }
