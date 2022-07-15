3f5f437f3e5fb33cd852d0e79bcf8019547bc423
Gautham Vanjre <vanjregautham@gmail.com>
2022-07-15 20:43:29 +0530

    completed filtering feature

--- app/src/main/java/com/example/notetakingapp/ui/Fragments/EditNotesFragment.kt
+++ app/src/main/java/com/example/notetakingapp/ui/Fragments/EditNotesFragment.kt
@@ -2,10 +2,9 @@ package com.example.notetakingapp.ui.Fragments
 
 import android.os.Bundle
 import android.text.format.DateFormat
+import android.view.*
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
@@ -14,6 +13,7 @@ import com.example.notetakingapp.R
 import com.example.notetakingapp.databinding.FragmentEditNotesBinding
 import com.example.notetakingapp.model.Notes
 import com.example.notetakingapp.viewmodel.NotesViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import java.util.*
 
 
@@ -30,6 +30,7 @@ class EditNotesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentEditNotesBinding.inflate(layoutInflater,container,false)
+        setHasOptionsMenu(true)
         binding.editTitle.setText(oldNotes.notesData.title)
         binding.editSubTitle.setText(oldNotes.notesData.subTitle)
         binding.editNotes.setText(oldNotes.notesData.notes)
@@ -102,6 +103,34 @@ class EditNotesFragment : Fragment() {
         viewModel.updateNote(notesData)
         Toast.makeText(this.context,"Note updated successfully ", Toast.LENGTH_SHORT).show()
         Navigation.findNavController(it!!).navigate(R.id.action_editNotesFragment_to_homeFragment)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_delete){
+            val bottomSheet : BottomSheetDialog = BottomSheetDialog(requireContext())
+            bottomSheet.setContentView(R.layout.dialog_delete)
+            bottomSheet.show()
+
+            val textViewYes = bottomSheet.findViewById<TextView>(R.id.dialog_yes)
+            val textViewNo = bottomSheet.findViewById<TextView>(R.id.dialog_no)
+
+            textViewYes?.setOnClickListener {
+                viewModel.deleteNote(oldNotes.notesData.id!!)
+                bottomSheet.dismiss()
+            }
+
+            textViewNo?.setOnClickListener {
+                bottomSheet.dismiss()
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+
     }
 
 
--- app/src/main/java/com/example/notetakingapp/ui/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/notetakingapp/ui/Fragments/HomeFragment.kt
@@ -1,15 +1,17 @@
 package com.example.notetakingapp.ui.Fragments
 
 import android.os.Bundle
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.notetakingapp.R
 import com.example.notetakingapp.databinding.FragmentHomeBinding
+import com.example.notetakingapp.model.Notes
 import com.example.notetakingapp.ui.Adapter.NotesAdapter
 import com.example.notetakingapp.viewmodel.NotesViewModel
 
@@ -18,6 +20,8 @@ class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
     val viewModel : NotesViewModel by viewModels()
+    var myNotes  = arrayListOf<Notes>()
+    lateinit var notesAdapter: NotesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,35 +29,46 @@ class HomeFragment : Fragment() {
     ): View? {
 
         binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
+        setHasOptionsMenu(true)
         binding.btnAddNote.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_addNotesFragment)
         }
         viewModel.getNotes().observe(viewLifecycleOwner,{
             notesList ->
-                binding.rcvAllNotes.layoutManager = GridLayoutManager(requireContext(),2)
-                binding.rcvAllNotes.adapter = NotesAdapter(requireContext(),notesList)
+                val staggeredGridLayoutManager = StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL)
+                binding.rcvAllNotes.layoutManager = staggeredGridLayoutManager
+                notesAdapter = NotesAdapter(requireContext(),notesList)
+                binding.rcvAllNotes.adapter = notesAdapter
         })
 
         binding.allNotes.setOnClickListener {
             viewModel.getNotes().observe(viewLifecycleOwner,{
                     notesList ->
-                binding.rcvAllNotes.layoutManager = GridLayoutManager(requireContext(),2)
-                binding.rcvAllNotes.adapter = NotesAdapter(requireContext(),notesList)
+                myNotes = notesList as ArrayList<Notes>
+                val staggeredGridLayoutManager = StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL)
+                binding.rcvAllNotes.layoutManager = staggeredGridLayoutManager
+                notesAdapter = NotesAdapter(requireContext(),notesList)
+                binding.rcvAllNotes.adapter = notesAdapter
             })
         }
 
         binding.filterHigh.setOnClickListener {
             viewModel.getHighNotes().observe(viewLifecycleOwner,{
                     notesList ->
-                binding.rcvAllNotes.layoutManager = GridLayoutManager(requireContext(),2)
-                binding.rcvAllNotes.adapter = NotesAdapter(requireContext(),notesList)
+                myNotes = notesList as ArrayList<Notes>
+                val staggeredGridLayoutManager = StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL)
+                binding.rcvAllNotes.layoutManager = staggeredGridLayoutManager
+                notesAdapter = NotesAdapter(requireContext(),notesList)
+                binding.rcvAllNotes.adapter = notesAdapter
             })
         }
 
         binding.filterMedium.setOnClickListener {
             viewModel.getMediumNotes().observe(viewLifecycleOwner,{
                     notesList ->
-                binding.rcvAllNotes.layoutManager = GridLayoutManager(requireContext(),2)
+                myNotes = notesList as ArrayList<Notes>
+                val staggeredGridLayoutManager = StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL)
+                binding.rcvAllNotes.layoutManager = staggeredGridLayoutManager
                 binding.rcvAllNotes.adapter = NotesAdapter(requireContext(),notesList)
             })
         }
@@ -61,12 +76,44 @@ class HomeFragment : Fragment() {
         binding.filterLow.setOnClickListener {
             viewModel.getLowNotes().observe(viewLifecycleOwner,{
                     notesList ->
-                binding.rcvAllNotes.layoutManager = GridLayoutManager(requireContext(),2)
+                myNotes = notesList as ArrayList<Notes>
+                val staggeredGridLayoutManager = StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL)
+                binding.rcvAllNotes.layoutManager = staggeredGridLayoutManager
                 binding.rcvAllNotes.adapter = NotesAdapter(requireContext(),notesList)
             })
         }
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu,menu)
+
+        val item = menu.findItem(R.id.app_bar_search)
+        val searchView = item.actionView as SearchView
+        searchView.queryHint = "Enter Notes here... "
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                NotesFiltering(p0)
+                return true
+            }
+        })
+
+        super.onCreateOptionsMenu(menu, inflater)
+
+    }
+
+    private fun NotesFiltering(p0 : String?) {
+        val newFilteredList = arrayListOf<Notes>()
+        for(i in myNotes){
+            if(i.title.contains(p0!!) || i.subTitle.contains(p0!!)){
+                newFilteredList.add(i)
+            }
+        }
     }
 
 
